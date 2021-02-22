44bb738df51301bda6422dfe07c2aff38d49bbf8
shuye <78575544+shuye73@users.noreply.github.com>
2021-02-22 10:46:01 +0800

    新增华硕爱奇艺（只做带京豆任务，无助力系统）
    新增华硕爱奇艺（只做带京豆任务，无助力系统）

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -14,6 +14,8 @@
 9 0,12,18 * * * node /scripts/jd_nzmh.js >> /scripts/logs/jd_nzmh.log 2>&1
 # 美丽研究院
 9 0,12,18 * * * node /scripts/jd_beauty.js >> /scripts/logs/jd_beauty.log 2>&1
+# 华硕爱奇艺
+24 0,12,18 * * * node /scripts/jd_asus_iqiyi.js >> /scripts/logs/jd_asus_iqiyi.log 2>&1
 ##############长期活动##############
 # 签到
 0 0,18 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
