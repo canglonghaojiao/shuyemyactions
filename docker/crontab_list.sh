c74fb6c4d8a78580d4d2b18145377a5b952793a3
shuye <78575544+shuye73@users.noreply.github.com>
2021-02-23 14:38:22 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -10,6 +10,10 @@
 29 0,12,18 * * * node /scripts/jd_fanslove.js >> /scripts/logs/jd_fanslove.log 2>&1
 # 京东女装盲盒
 9 0,12,18 * * * node /scripts/jd_nzmh.js >> /scripts/logs/jd_nzmh.log 2>&1
+# 华硕爱奇艺
+0 0,12,18 * * * node /scripts/jd_asus_iqiyi.js >> /scripts/logs/jd_asus_iqiyi.log 2>&1
+# 美丽研究院
+0 0,9,12 * * * node /scripts/jd_beauty.js >> /scripts/logs/jd_beauty.log 2>&1
 ##############长期活动##############
 # 签到
 0 0,18 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
